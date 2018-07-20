9018d8346ed03101caa2896e4159ea7a8cd9ad15
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-19 21:41:13 -0400

    pre rubocop

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development, :test do
   # Spring speeds up development by keeping your
   # application running in the background. Read more: https://github.com/rails/spring
   gem "capybara"
+  gem "pry"
   gem "rubocop", "0.58.1"
   gem "rspec-rails", "~> 3.7"
   gem "spring"
--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,38 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    end
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+  def note_contents=(note_content)
+    note_content.each do |content|
+      unless content.empty?
+        note = Note.find_or_create_by(content: content)
+        notes << note
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map{ |note| note.content }
+  end
 end
