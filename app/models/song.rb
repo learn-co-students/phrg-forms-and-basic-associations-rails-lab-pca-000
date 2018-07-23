c2d5731ad6d49e5b0455e08ee9ac33f8ceba4008
bkrigel <benjamin.krigel@gmail.com>
2018-07-23 16:55:25 -0400

    Learn you dirty dirty sob

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,30 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist&.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    genre&.name
+  end
+
+  def note_contents=(contents)
+    contents.reject(&:blank?).each do |content|
+      notes.build(content: content)
+    end
+  end
+
+  def note_contents
+    notes.map(&:content)
+  end
 end
